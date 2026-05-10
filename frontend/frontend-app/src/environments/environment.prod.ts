faf5beac929e5801e2345ba368373463f995ab64
Bonnieee17 <bonalynjane17@gmail.com>
2026-05-11 00:51:31 +0800

    fix corrupted environments files with correct API URL

--- frontend/frontend-app/src/environments/environment.prod.ts
+++ frontend/frontend-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://document-request-system-3w9u.onrender.com'
+  apiUrl: "https://document-request-system-3w9u.onrender.com/api"
 };
--- frontend/frontend-app/src/environments/environment.ts
+++ frontend/frontend-app/src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDaOQ0sFTprlIEG3aiVMwGgqFheI6-rFRg',
-    authDomain: 'document-request-system-53127.firebaseapp.com',
-    projectId: 'document-request-system-53127',
-    storageBucket: 'document-request-system-53127.firebasestorage.app',
-    messagingSenderId: '343905608788',
-    appId: '1:343905608788:web:fef2940ad27a19248ef9c1',
-    measurementId: 'G-PL6BGNM3MT'
+    apiKey: "AIzaSyDaOQ0sFTprlIEG3aiVMwGgqFheI6-rFRg",
+    authDomain: "document-request-system-53127.firebaseapp.com",
+    projectId: "document-request-system-53127",
+    storageBucket: "document-request-system-53127.firebasestorage.app",
+    messagingSenderId: "343905608788",
+    appId: "1:343905608788:web:fef2940ad27a19248ef9c1",
+    measurementId: "G-PL6BGNM3MT"
   },
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: "http://localhost:5000/api"
 };
