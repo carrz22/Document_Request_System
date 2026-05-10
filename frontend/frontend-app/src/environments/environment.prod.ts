bb5cafb28ee31e4b22b925ed43840bfddccce4bd
carrz22 <your-carrz22-email@gmail.com>
2026-05-11 01:31:00 +0800

    fix environment prod with firebase config and render url

--- frontend/frontend-app/src/environments/environment.prod.ts
+++ frontend/frontend-app/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: "https://document-request-system-3w9u.onrender.com/api"
+  firebaseConfig: {
+    apiKey: 'AIzaSyDaOQ0sFTprlIEG3aiVMwGgqFheI6-rFRg',
+    authDomain: 'document-request-system-53127.firebaseapp.com',
+    projectId: 'document-request-system-53127',
+    storageBucket: 'document-request-system-53127.firebasestorage.app',
+    messagingSenderId: '343905608788',
+    appId: '1:343905608788:web:fef2940ad27a19248ef9c1',
+    measurementId: 'G-PL6BGNM3MT'
+  },
+  apiUrl: 'https://document-request-system-3w9u.onrender.com/api'
 };
